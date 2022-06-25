7b2a0a06da34bf1b3ede6b0de04ba33b4f8ccfce
Bittoo Aggarwal <bitz1119@gmail.com>
2022-06-25 12:06:23 +0530

    todoApp

--- Class-13/ToDoApp/script.js
+++ Class-13/ToDoApp/script.js
@@ -5,6 +5,7 @@ class ToDo{
         this.id = todoArray.length;
         this.text = todo;
         this.isDone = false;
+        this.isDeleted = false;
     }
 }
 
@@ -14,6 +15,7 @@ function createToDo(){
         let newToDo = new ToDo(textarea.value);
         todoArray.push(newToDo);
         renderUI();
+        // clear text area after adding todo
         textarea.value = "";
     }
     else{
@@ -24,21 +26,32 @@ function createToDo(){
 function renderUI(){
     let tableBody = document.getElementById("table-body");
     // put all todoArray element in tbody
-    tbody = "";
+    tbodyText = "";
     for(let i = 0; i < todoArray.length;i++){
-        tbody += `  <tr class="table-active">
+        tbodyText = tbodyText + `  <tr class="table-active" style="${todoArray[i].isDeleted == true ? "display:none" : ""}">
         <td>
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ${todoArray[i].isDone === true ? "checked" :""} />
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ${todoArray[i].isDone === true ? "checked" :""} onclick="taskDone(${todoArray[i].id})"/>
             </div>
           </td>
           <td>
             ${todoArray[i].text}
           </td>
+          <td>
+          <button type="button" class="btn btn-danger" onclick="deleteTodo(${todoArray[i].id})">Delete</button>
+          </td>
       </tr>`
     }
 
-    tableBody.innerHTML = tbody;
-
+    tableBody.innerHTML = tbodyText;
+    
 }
 
+function taskDone(id){
+    todoArray[id].isDone = true;
+}
+
+function deleteTodo(id){
+    todoArray[id].isDeleted = true;
+    renderUI();
+}
