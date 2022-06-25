8378eac8155745dcf8ec22f7682141721f023a0a
Bittoo Aggarwal <bitz1119@gmail.com>
2022-06-25 11:10:08 +0530

    todoApp

--- Class-13/ToDoApp/script.js
+++ Class-13/ToDoApp/script.js
@@ -8,15 +8,37 @@ class ToDo{
     }
 }
 
-function createToDo(text){
-    let newToDo = new ToDo(text);
-    todoArray.push(newToDo);
+function createToDo(){
+    let textarea = document.getElementById("text-area");
+    if(textarea.value.length !== 0){
+        let newToDo = new ToDo(textarea.value);
+        todoArray.push(newToDo);
+        renderUI();
+        textarea.value = "";
+    }
+    else{
+        alert("Please enter something");
+    }
 }
 
 function renderUI(){
     let tableBody = document.getElementById("table-body");
     // put all todoArray element in tbody
+    tbody = "";
+    for(let i = 0; i < todoArray.length;i++){
+        tbody += `  <tr class="table-active">
+        <td>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" ${todoArray[i].isDone === true ? "checked" :""} />
+            </div>
+          </td>
+          <td>
+            ${todoArray[i].text}
+          </td>
+      </tr>`
+    }
 
+    tableBody.innerHTML = tbody;
 
 }
 
